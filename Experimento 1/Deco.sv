4079337b8d8fb6d53d7b182de659bae00bd19270
Cris_Azofeifa <32116409+CrisAzofeifa@users.noreply.github.com>
2022-03-06 19:35:45 -0600

    Exp1 testbench

--- Experimento 1/Deco.sv
+++ Experimento 1/Deco.sv
@@ -1,12 +1,12 @@
 module Deco(input logic A,B,C,D,E,
-				output logic G,H,I,J,K,L,M);
+				output logic [0:6] hex);
 
-		assign G = (~B&C&D&E)|(~A&B&~C&~D)|(~A&B&~C&E)|(A&~B&C&~D&~E)|(A&B&C&~D&E);
-		assign H = (~B&~C&E)|(~A&~C&D)|(~B&~C&D)|(~A&D&~E)|(~A&C&~D)|(~A&B&~E)|(A&~C&~D)|(A&B&C&D);
-		assign I = (~B&~D&E)|(~A&~C&D)|(~A&D&~E)|(~A&C&~D)|(~A&B&~E)|(B&C&D)|(A&~B&~C);
-		assign J = (~A&~C&~D&~E)|(~A&~B&C&E)|(~A&C&D&E)|(~B&C&D&E)|(A&~B&C&~D&~E)|(A&B&~C&D&~E);
-		assign K = (A&C&~D&~E)|(A&~B&C&D)|(A&B&~C&~E)|(~A&B&~C&~D&E);
-		assign L = (A&~B&C&~D)|(A&~B&C&~E)|(~A&B&~C&~D&E)|(A&B&~C&D&~E);
-		assign M = (~A&B&~C&~D)|(~A&B&~C&E)|(~A&~B&~C&D&~E)|(A&~B&C&~D&~E)|(A&B&~C&D&~E)|(A&B&C&~D&E);
+		assign hex[0] = (~A & ~C & E ) | ( ~B & C & D & E ) | ( ~A & B & ~C & ~D ) | ( A & ~B & C & ~D & ~E ) | ( A & B & C & ~D & E);
+		assign hex[1] = (~A & C & ~D ) | ( A & ~B & ~C ) | ( A & ~C & ~D ) | ( ~A & ~B & ~D & E ) | ( ~A & ~B & D & ~E ) | ( ~A & B & ~C & ~E ) | ( ~A & B & ~C & D ) | ( A & B & C & D & ~E);
+		assign hex[2] = (~A & C & ~D ) | ( ~A & ~C & D & ~E ) | ( ~B & C & ~D & E ) | ( ~A & B & ~C & ~E ) | ( ~A & B & D & E ) | ( A & ~B & ~C & ~D ) | ( A & ~B & ~C & E ) | ( A & B & C & D);
+		assign hex[3] = (~A & ~C & ~D & ~E ) | ( ~A & ~B & C & E ) | ( ~A & C & D & E ) | ( ~B & C & D & E ) | ( A & ~B & C & ~D & ~E ) | ( A & B & ~C & D & ~E);
+		assign hex[4] = (A & ~B & D & ~E ) | ( A & C & ~D & ~E ) | ( A & ~B & C & D ) | ( A & B & ~C & ~E ) | ( ~A & B & ~C & ~D & E);
+		assign hex[5] = (A & ~B & C & ~D ) | ( A & ~B & C & ~E ) | ( ~A & ~B & ~C & D & E ) | ( ~A & B & ~C & ~D & E ) | ( A & B & ~C & D & ~E ) | ( A & B & C & D & E);
+		assign hex[6] = (~A & B & ~C & ~D ) | ( ~A & B & ~C & E ) | ( ~A & ~B & ~C & D & ~E ) | ( ~A & B & C & D & ~E ) | ( A & ~B & C & ~D & ~E ) | ( A & B & ~C & D & ~E ) | ( A & B & C & ~D & E);
 
 endmodule
--- Experimento 1/output_files/Deco.cdf
+++ Experimento 1/output_files/Deco.cdf
@@ -6,7 +6,7 @@ JedecChain;
 	P ActionCode(Ign)
 		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
 	P ActionCode(Cfg)
-		Device PartName(5CSEMA5F31) Path("C:/Users/azofe/Desktop/Semestre I-2022/Taller De Digitales/Quartus WorkSpace/output_files/") File("Deco.sof") MfrSpec(OpMask(1));
+		Device PartName(5CSEMA5F31) Path("C:/Users/azofe/Desktop/Semestre I-2022/Taller De Digitales/Laboratorio 2/Experimento 1/output_files/") File("Deco.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
